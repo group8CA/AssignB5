f4da7ff324b0e4b50632b33cc45de99634f35ae3
Rohan Bhatt <rb954@scarletmail.rutgers.edu>
2020-04-25 17:47:36 -0400

    update scripts

---
+++ Assets/DoorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    public GameObject Door;
+    public bool doorIsOpening;
+
+    void Update()
+    {
+        if (doorIsOpening == true)
+        {
+            Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
+            //if the bool is true open the door
+
+        }
+        if (Door.transform.position.y > 7f)
+        {
+            doorIsOpening = false;
+            //if the y of the door is > than 7 we want to stop the door
+        }
+    }
+    void OnMouseDown()
+    { //THIS FUNCTION WILL DETECT THE MOUSE CLICK ON A COLLIDER,IN OUR CASE WILL DETECT THE CLICK ON THE BUTTON
+
+        doorIsOpening = true;
+        //if we click on the button door we must start to open
+    }
+
+}
